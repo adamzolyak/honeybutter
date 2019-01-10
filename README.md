7b8c26362e0b8cb804addf0d839b76d9a5b9632d
Adam Zolyak <adam.zolyak@gmail.com>
2019-01-10 16:38:30 -0700

    v1

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Sugar
+# Butter for Waffle.io
 
-More toppings for Waffle.io!
+Butter is a Chrome Extension that enables you to create and use Issue templates in Waffle.io! Templates are the 3rd [most requested feature](https://github.com/waffleio/waffle.io/issues/2398) in Waffle.io.
+
+## How It Works
+
+Butter adds a template drop down to the Add Issue form in Waffle.io projects. You can select a template when creating an issue which will replace the issue descripton with the template. Templates are defined in this extension. In the future, the extension might use [GitHub Issue Templates](https://help.github.com/articles/manually-creating-a-single-issue-template-for-your-repository/).
+
+## Installation
+
+## Examples
+
+## Extending
+
+1. Clone this repo.
+2. See [Chrome Extensions - Getting Started](https://developer.chrome.com/extensions/getstarted) to learn how to install and run this extension locally. Make sure you refresh the extention after make changes!
--- contentScript.js
+++ contentScript.js
@@ -1,26 +1,22 @@
-//doesn't run until the webpage is already loaded
+//define templates
+//TODO: pull templates from GitHub and may also support labels
+const templateUserStory = `# User Story
 
-var delayInMilliseconds = 3000 //1 second
+As a [persona], I need [some feature] so that [I get some benefit].
 
-const templateUserStory = `
-**User Story**
-
-As a <persona>, I need <some feature> So That <I get some benefit>.
-
-Acceptance Criteria
+## Acceptance Criteria
 - [ ] criteria
 - [ ] criteria
 - [ ] criteria
 `
 
-const templateEpic = `
-**Epic**
+const templateEpic = `# Epic
 
-Objective
+## Objective
 
-Research
+## Research
 
-Acceptance Criteria
+## Acceptance Criteria
 - [ ] criteria
 - [ ] criteria
 - [ ] criteria
@@ -30,102 +26,104 @@ parent of #<issueNumber>
 parent of #<issueNumber>
 `
 
-const templateBug = `
-**Bug**
+const templateBug = `# Bug
 
-Expected Result
+## Expected Result
 
-Actual Result
+## Actual Result
 
-Steps to Reproduce
+## Steps to Reproduce
 
-Support Cases
+## Support Cases
 `
+
+//wait until Waffle.io data is loaded
+//TODO: dynamically handle this case
+var delayInMilliseconds = 3000
 
 setTimeout(function() {
   console.log('Waffle.io Sugar is running!')
 
+  //visually indicate the extension is running by coloring card headers
   const cards = document.getElementsByClassName('card-header')
 
   for (card of cards) {
     card.style['background-color'] = '#FFFF00'
   }
 
+  //add template selector to add issue form
   const addHTML = `
-  <span class="js-source-select" style="display: block;margin-bottom:10px;">
-    <i class="github-icon github-icon-pencil select-icon"></i>
-    <div class="dropdown select select-with-icon" tabindex="0">
-    <a class="dropdown-menu-toggle" data-toggle="dropdown">
-        <div class="dropdown-title template-options-title">Select Template</div>
-        <i class="fa fa-angle-down toggle-caret"></i>
-    </a>
-    <div class="dropdown-menu">
-        <div class="dropdown-input-ct">
-            <input type="text" placeholder="Select template" class="form-control dropdown-input ">
+    <span class="js-source-select" style="display: block;margin-bottom:10px;">
+        <i class="github-icon github-icon-pencil select-icon"></i>
+        <div class="dropdown select select-with-icon" tabindex="0">
+        <a class="dropdown-menu-toggle" data-toggle="dropdown">
+            <div class="dropdown-title template-options-title">Select Template</div>
+            <i class="fa fa-angle-down toggle-caret"></i>
+        </a>
+        <div class="dropdown-menu">
+            <div class="dropdown-input-ct">
+                <input type="text" placeholder="Select template" class="form-control dropdown-input ">
+            </div>
+            <ul class="dropdown-menu-item-list">
+                <li class="dropdown-menu-item template-option">
+                    <span class="source-title">no template</span>
+                </li>
+                <li class="dropdown-menu-item template-option">
+                    <span class="source-title">User Story Template</span>
+                </li>
+                <li class="dropdown-menu-item template-option">
+                    <span class="source-title">Epic Template</span>
+                </li>
+                <li class="dropdown-menu-item template-option">
+                    <span class="source-title">Bug Template</span>
+                </li>
+            </ul>
+            </div>
         </div>
-        <ul class="dropdown-menu-item-list">
-            <li class="dropdown-menu-item template-option">
-                <span class="source-title">no template</span>
-            </li>
-            <li class="dropdown-menu-item template-option">
-                <span class="source-title">User Story Template</span>
-            </li>
-            <li class="dropdown-menu-item template-option">
-                <span class="source-title">Epic Template</span>
-            </li>
-            <li class="dropdown-menu-item template-option">
-                <span class="source-title">Bug Template</span>
-            </li>
-        </ul>
-        </div>
-    </div>
-</span>
-`
+    </span>
+    `
 
-  const newCardForm = document.getElementsByClassName(
+  const newCardFormTitle = document.getElementsByClassName(
     'form-control text-input ng-pristine ng-valid'
   )[0]
 
-  const newOuterHTML = newCardForm.outerHTML + addHTML
+  newCardFormTitle.insertAdjacentHTML('beforebegin', addHTML)
 
-  newCardForm.outerHTML = newOuterHTML
-
+  //add onClick listeners to options in template selector
   const templateOptions = document.getElementsByClassName('template-option')
-
-  console.log('templateOptions', templateOptions)
 
   for (templateOption of templateOptions) {
     templateOption.addEventListener('click', event => {
-      console.log('click', event.toElement.innerText)
       updateTemplate(event.toElement.innerText)
     })
   }
 
+  //reset template slector to no template after creating new issue
+  const addNewButton = document.getElementsByClassName('js-add-new-button')[0] //TODO: remove hardcoded array position
+
+  addNewButton.addEventListener('click', () => {
+    document.getElementsByClassName('template-options-title')[0].innerText =
+      'no template'
+  }) //TODO: remove hardcoded array position
+
+  //replace card description with template when teplate is selected
   function updateTemplate(selectedTemplate) {
-    const { children } = document.getElementsByClassName('card-description')[0]
-    //hardcoded - TODO
-    let cardDescription = children[0]
+    const { children } = document.getElementsByClassName('card-description')[0] //TODO: remove hardcoded array position
+    let cardDescription = children[0] //TODO: remove hardcoded array position
 
     let templateSelectorTitle = document.getElementsByClassName(
       'template-options-title'
     )[0]
 
-    console.log('templateSelectorTitle', templateSelectorTitle)
-
     if (selectedTemplate.toLowerCase() == 'user story template') {
-      console.log('using user story template...')
       templateSelectorTitle.innerText = selectedTemplate
       cardDescription.value = templateUserStory
     } else if (selectedTemplate.toLowerCase() == 'epic template') {
-      console.log('using epic template...')
       templateSelectorTitle.innerText = selectedTemplate
       cardDescription.value = templateEpic
     } else if (selectedTemplate.toLowerCase() == 'bug template') {
-      console.log('using bug template...')
       templateSelectorTitle.innerText = selectedTemplate
       cardDescription.value = templateBug
-    } else {
-      console.log('no template for selected template')
     }
   }
 }, delayInMilliseconds)
